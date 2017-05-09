68685f332e8c76b9944c6b4fae533504a1e992c4
Alex J. Malozemoff <amaloz@galois.com>
2017-05-09 16:32:22 -0700

    update point fn experiment

--- experiments/point.sh
+++ experiments/point.sh
@@ -6,10 +6,12 @@ mio=$(readlink -f "$dir/../mio")
 
 circuit="$circuits/point/point_base10.dsl.acirc"
 
-$mio obf obfuscate --verbose --base 10 --smart "$circuit" 2>&1 | tee /tmp/obfuscate.txt
+args="--verbose --base 10 --smart --mmap CLT --secparam 80 $circuit"
+
+$mio obf obfuscate $args 2>&1 | tee /tmp/obfuscate.txt
 obf_time=$(tail -1 /tmp/obfuscate.txt | cut -d' ' -f3)
 obf_size=$(ls -lh "$circuit.obf" | cut -d' ' -f5)
-$mio obf evaluate --verbose --base 10 "$circuits/point/point_base10.dsl.acirc" 0000000000000000000000000 2>&1 | tee /tmp/evaluate.txt
+$mio obf evaluate $args 0000000000000000000000000 2>&1 | tee /tmp/evaluate.txt
 eval_time=$(cat /tmp/evaluate.txt | grep "evaluate total" | cut -d' ' -f3)
 
 echo ""
